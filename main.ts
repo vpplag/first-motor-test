212f41781deb90e4b2c531c87522981fc427382c
vpplag <74141971+vpplag@users.noreply.github.com>
2021-04-08 00:27:22 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,9 +4,6 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showNumber(speed / 10)
 })
-input.onButtonPressed(Button.AB, function () {
-    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, speed)
-})
 input.onButtonPressed(Button.B, function () {
     if (speed < 100) {
         speed += 10
@@ -16,3 +13,6 @@ input.onButtonPressed(Button.B, function () {
 let speed = 0
 speed = 0
 basic.showNumber(speed)
+basic.forever(function () {
+    Kitronik_Robotics_Board.motorOn(Kitronik_Robotics_Board.Motors.Motor1, Kitronik_Robotics_Board.MotorDirection.Forward, speed)
+})
